91abacce8fbe2be240810ce9f0caa0d83288f33c
Timothy Miamba <timal0361@gmail.com>
2024-08-23 17:37:45 +0200

    feat: Add Button component

---
+++ components/Button.js
@@ -0,0 +1,40 @@
+import { StyleSheet, View, Pressable, Text } from "react-native-web";
+
+export default function Button({ label }) {
+  return (
+    <View styles={styles.buttonContainer}>
+      <Pressable
+        styles={styles.button}
+        onPress={() => alert("You pressed a button")}
+      >
+        <Text styles={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
