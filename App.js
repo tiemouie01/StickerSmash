8a7cb01521ed7d7e3971247bbd93a39956a71431
Timothy Miamba <timal0361@gmail.com>
2024-08-23 11:35:27 +0200

    feat: Add default image to App

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const PlaceHolderImage = require("./assets/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={PlaceHolderImage} style={styles.image} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
