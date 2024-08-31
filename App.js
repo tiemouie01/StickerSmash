a0ab7b48fae99b9a91e48547d3185504146b26d4
Timothy Miamba <timal0361@gmail.com>
2024-08-31 17:13:19 +0200

    feat: Add Image Picker functionality

--- App.js
+++ App.js
@@ -4,14 +4,32 @@ import { StyleSheet, View } from "react-native";
 import Button from "./components/Button";
 import ImageViewer from "./components/ImageViewer";
 
+import * as ImagePicker from "expo-image-picker";
+
 const PlaceHolderImage = require("./assets/background-image.png");
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not select any image");
+    }
+  };
   return (
     <View style={styles.container}>
       <ImageViewer placeholderImageSource={PlaceHolderImage} />
       <View style={styles.footerContainer}>
-        <Button theme={"primary"} label={"Choose a photo"} />
+        <Button
+          onPress={pickImageAsync}
+          theme={"primary"}
+          label={"Choose a photo"}
+        />
         <Button label={"Use this photo"} />
       </View>
       <StatusBar style="auto" />
--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,13 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
   if (theme === "primary") {
     return (
       <View style={styles.buttonContainer}>
         <Pressable
           style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert("Be downbad like Chichindami.")}
+          onPress={onPress}
         >
           <FontAwesome
             name="picture-o"
