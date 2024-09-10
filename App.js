ff902bcf9fdd876b671f494b3fe204df6ee69fc6
Timothy Miamba <timal0361@gmail.com>
2024-09-10 20:17:45 +0200

    feat: Add screenshot capture functionality

--- App.js
+++ App.js
@@ -56,7 +56,19 @@ export default function App() {
   };
 
   const onSaveImageSync = async () => {
-    // Shhhh.
+    try {
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri) {
+        alert("Saved!");
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
