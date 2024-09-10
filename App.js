d97eddc8a45af1279c2cbf7fad38089b127aed1e
Timothy Miamba <timal0361@gmail.com>
2024-09-10 20:05:42 +0200

    feat: Prompt for permissions

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as MediaLibrary from "expo-media-library";
 
 import Button from "./components/Button";
 import ImageViewer from "./components/ImageViewer";
@@ -20,6 +21,11 @@ export default function App() {
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  if (status === null) {
+    requestPermission();
+  }
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
